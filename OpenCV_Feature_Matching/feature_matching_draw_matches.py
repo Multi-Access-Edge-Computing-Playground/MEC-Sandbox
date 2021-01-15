5d78da02d675fe4e1771c7599e23f9479aaf746a
Fabian Jonen <fabian.jonen@gmail.com>
2021-01-15 14:31:53 +0100

    some cleanup

--- OpenCV_Feature_Matching/feature_matching_draw_matches.py
+++ OpenCV_Feature_Matching/feature_matching_draw_matches.py
@@ -6,13 +6,10 @@ img1 = cv2.imread('./training.jpg',0)# queryImage
 img2 = cv2.imread('./scene.jpg',0) # trainImage
 
 # Initiate SIFT detector
-print("1")
 orb = cv2.ORB_create()
-print("2")
 # find the keypoints and descriptors with SIFT
 kp1, des1 = orb.detectAndCompute(img1,None)
 kp2, des2 = orb.detectAndCompute(img2,None)
-print("3")
 # create BFMatcher object
 bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
@@ -26,5 +23,3 @@ matches = sorted(matches, key = lambda x:x.distance)
 img3 = cv2.drawMatches(img1,kp1,img2,kp2,matches,None, flags=2)
 cv2.imshow("Detections", img3)
 cv2.waitKey(0)
-# plt.imshow(img3),plt.show()
-print("thanks, i am done")
