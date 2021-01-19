cdc76b80f0d138840f37e486943cb77eb16db99f
mantheyjulius <72564273+mantheyjulius@users.noreply.github.com>
2021-01-19 19:37:15 +0100

    added method order_points

--- Projects/QR_Code/qr_code_detector.py
+++ Projects/QR_Code/qr_code_detector.py
@@ -13,6 +13,30 @@ fontStyle = cv2.FONT_HERSHEY_SIMPLEX
 fontScale = 0.7
 fontThickness = 2
 t0=time.time()
+
+
+def order_points(pts):
+	# initialzie a list of coordinates that will be ordered
+	# such that the first entry in the list is the top-left,
+	# the second entry is the top-right, the third is the
+	# bottom-right, and the fourth is the bottom-left
+	rect = np.zeros((4, 2), dtype = "float32")
+	# the top-left point will have the smallest sum, whereas
+	# the bottom-right point will have the largest sum
+	s = pts.sum(axis = 1)
+	rect[0] = pts[np.argmin(s)]
+	rect[2] = pts[np.argmax(s)]
+	# now, compute the difference between the points, the
+	# top-right point will have the smallest difference,
+	# whereas the bottom-left will have the largest difference
+	diff = np.diff(pts, axis = 1)
+	rect[1] = pts[np.argmin(diff)]
+	rect[3] = pts[np.argmax(diff)]
+	# return the ordered coordinates
+	return rect
+
+
+
 while True:
     image = cap.read()
     # image = cv2.imread('testQRCode2.png')
@@ -25,39 +49,38 @@ while True:
             nextPointIndex = (i0+1) % nrOfPoints
             cv2.line(image, tuple(points[0][i0]), tuple(points[0][nextPointIndex]), (255,0,0), 5)
         print(decodedText)
-        # rect = order_points(pts)
-        #(tl, tr, br, bl) = points[0]
+        rect = order_points(pts)
+        (tl, tr, br, bl) = points[0]
         # compute the width of the new image, which will be the
         # maximum distance between bottom-right and bottom-left
-        # x-coordiates or the top-right and top-left x-coordinates
-        #widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
-        #widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
-        #maxWidth = max(int(widthA), int(widthB))
-        # compute the height of the new image, which will be the
-        # maximum distance between the top-right and bottom-right
-        # y-coordinates or the top-left and bottom-left y-coordinates
-        #heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
-        #heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
-        #maxHeight = max(int(heightA), int(heightB))
-        # now that we have the dimensions of the new image, construct
-        # the set of destination points to obtain a "birds eye view",
-        # (i.e. top-down view) of the image, again specifying points
-        # in the top-left, top-right, bottom-right, and bottom-left
-        # order
-        # dst = np.array([
-        #    [0, 0],
-        #    [maxWidth - 1, 0],
-        #    [maxWidth - 1, maxHeight - 1],
-        #    [0, maxHeight - 1]], dtype = "float32")
-        #M = cv2.getPerspectiveTransform(points[0], dst)
-        # https://stackoverflow.com/questions/60138100/calculate-pitch-and-roll-and-yaw-by-4-points-detected-from-a-square-in-opencv
-        #print(M)
-        #constellations = ["xyz","xzy","yxz","yzx","zxy","zyx","XYZ","XZY","YXZ","YZX","ZXY","ZYX"]
-        #for const in constellations:
-        #    euler = R.from_matrix(M).as_euler(const,degrees=True)
-        #    # print(round(euler,2))
-        #    print([round(elem,2) for elem in euler])
-        #sys.exit()
+         x-coordiates or the top-right and top-left x-coordinates
+        widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+        widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+        maxWidth = max(int(widthA), int(widthB))
+         #compute the height of the new image, which will be the
+         #maximum distance between the top-right and bottom-right
+         y-coordinates or the top-left and bottom-left y-coordinates
+        heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+        heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+        maxHeight = max(int(heightA), int(heightB))
+         #now that we have the dimensions of the new image, construct
+         #the set of destination points to obtain a "birds eye view",
+         #(i.e. top-down view) of the image, again specifying points
+         #in the top-left, top-right, bottom-right, and bottom-left
+         #order
+         dst = np.array([
+            [0, 0],
+            [maxWidth - 1, 0],
+            [maxWidth - 1, maxHeight - 1],
+            [0, maxHeight - 1]], dtype = "float32")
+        M = cv2.getPerspectiveTransform(points[0], dst)
+        print(M)
+        constellations = ["xyz","xzy","yxz","yzx","zxy","zyx","XYZ","XZY","YXZ","YZX","ZXY","ZYX"]
+        for const in constellations:
+            euler = R.from_matrix(M).as_euler(const,degrees=True)
+            print(round(euler,2))
+            print([round(elem,2) for elem in euler])
+        sys.exit()
     t1=time.time()
     frame_count+=1
     if t1-t0>=1.0:
@@ -69,4 +92,5 @@ while True:
     cv2.imshow("Image", image)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
 cv2.destroyAllWindows()
