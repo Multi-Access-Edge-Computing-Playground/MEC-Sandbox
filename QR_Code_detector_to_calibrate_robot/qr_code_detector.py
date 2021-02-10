bd1fa64aaa5400a3da8177a272a9cf80e55b92d8
mantheyjulius <72564273+mantheyjulius@users.noreply.github.com>
2021-02-10 13:15:44 +0100

    added methode

--- QR_Code_detector_to_calibrate_robot/qr_code_detector.py
+++ QR_Code_detector_to_calibrate_robot/qr_code_detector.py
@@ -5,6 +5,11 @@ from Subfunctions.WebcamVideoStream import WebcamVideoStream
 from pyimagesearch.transform import *
 from scipy.spatial.transform import Rotation as R
 import time
+from imutils import perspective
+from imutils import contours
+import argparse
+import imutils
+import cv2
 cap = WebcamVideoStream(1).start()
 time.sleep(0.5)
 frame_count=0
@@ -35,6 +40,27 @@ def find_draw_contours_calculate_px_pos_angle(imgThres,imgAnnotated):
 
 
     return imgAnnotated, obj_middle_pts_list, obj_angle_list
+
+
+def orderPoints(pts):
+	# initialize a list of coordinates that will be ordered
+	# such that the first entry in the list is the top-left,
+	# the second entry is the top-right, the third is the
+	# bottom-right, and the fourth is the bottom-left
+	rect = np.zeros((4, 2), dtype="float32")
+	# the top-left point will have the smallest sum, whereas
+	# the bottom-right point will have the largest sum
+	s = pts.sum(axis=1)
+	rect[0] = pts[np.argmin(s)]
+	rect[2] = pts[np.argmax(s)]
+	# now, compute the difference between the points, the
+	# top-right point will have the smallest difference,
+	# whereas the bottom-left will have the largest difference
+	diff = np.diff(pts, axis=1)
+	rect[1] = pts[np.argmin(diff)]
+	rect[3] = pts[np.argmax(diff)]
+	# return the ordered coordinates
+	return rect
 
 def matrix(imgThres,imgAnnotated):
     contours, hierarchy = cv2.findContours(imgThres, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
